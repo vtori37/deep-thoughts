ae4e4845480df11db5a39844ec45e94eabe46eca
Tori Acer <vrice360@gmail.com>
2022-06-28 17:25:54 -0500

    add client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,26 +6,27 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-      const userData = await User.findOne({ _id: context.user._id })
-      .select('-__v -password')
-      .populate('thoughts')
-      .populate('friends');
-      return userData;
-    }
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
 
-    throw new AuthenticationError('Not logged in');
-  },
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
     users: async () => {
       return User.find()
-      .select('-__v -password')
-      .populate('thoughts')
-      .populate('friends');
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-      .select('-__v -password')
-      .populate('friends')
-      .populate('thoughts');
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
     },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -33,7 +34,7 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
-    },
+    }
   },
 
   Mutation: {
@@ -102,4 +103,5 @@ const resolvers = {
     }
   }
 };
+
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -16,6 +17,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(epress.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
  
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
