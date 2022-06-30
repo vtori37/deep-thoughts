e5afb36ee21c5dc5f4aa5e13fb50bc33129022e1
Tori Acer <vrice360@gmail.com>
2022-06-29 22:10:15 -0500

    created and implemented user based queries

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
-  // update state based on form input changes
+  const [login, { error }] = useMutation(LOGIN_USER);
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -13,11 +15,16 @@ const Login = (props) => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
-
-    // clear form values
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      })
+     Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
     setFormState({
       email: '',
       password: '',
@@ -53,6 +60,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-
-  // update state based on form input changes
+  const [addUser, { error }] = useMutation(ADD_USER);
+ 
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -13,10 +16,20 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-  };
+
+const handleFormSubmit = async event => {
+  event.preventDefault();
+
+  try {
+    const { data } = await addUser({
+      variables: { ...formState }
+    });
+  
+    Auth.login(data.addUser.token);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -56,6 +69,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
